bd8a2fa308a284865f0fb99f5d04a7c30112438c
jaguililla <juanjoaguililla@gmail.com>
2024-09-29 12:35:48 +0200

    Fix tests

--- src/test/java/com/github/jaguililla/appointments/GatlingSimulation.java
+++ src/test/java/com/github/jaguililla/appointments/GatlingSimulation.java
@@ -2,19 +2,24 @@ package com.github.jaguililla.appointments;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
+import static java.util.UUID.randomUUID;
 
 import io.gatling.javaapi.core.*;
+
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class GatlingSimulation extends Simulation {
 
     private ChainBuilder appointmentsCrud = exec(
         http("Create")
             .post("/appointments")
+            .header("Content-Type", "application/json")
             .body(StringBody("""
                 {
-                  "users": [],
-                  "startTimestamp": "2024-09-28T21:28:00.419367341",
-                  "endTimestamp": "2024-09-28T21:28:00.4193957"
+                  "id": "#{id}",
+                  "startTimestamp": "2024-09-28T21:28:00",
+                  "endTimestamp": "2024-09-28T21:28:00"
                 }
                 """
             ))
@@ -33,7 +38,12 @@ public class GatlingSimulation extends Simulation {
     {
         var baseUrl = "http://localhost:18080";
         var httpProtocol = http.baseUrl(baseUrl);
-        var users = scenario("Appointments").exec(appointmentsCrud);
+        var users = scenario("Appointments")
+            .feed(Stream
+                .<Map<String, Object>>generate(() -> Map.of("id", randomUUID().toString()))
+                .iterator()
+            )
+            .exec(appointmentsCrud);
 
         setUp(users.injectOpen(rampUsers(10).during(10))).protocols(httpProtocol);
     }
