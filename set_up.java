7389d3f42d8a2aec49b47e10b358344ad8be71f9
jaguililla <juanjoaguililla@gmail.com>
2024-09-30 23:15:00 +0200

    Add set up script

--- set_up.java
+++ set_up.java
@@ -1,10 +1,28 @@
 ///usr/bin/env java --enable-preview --source 21 -cp "*" "$0" "$@" ; exit $?
 
+import static java.util.Map.entry;
+
+import java.io.Console;
+import java.util.List;
+
+private Console console = System.console();
+
 void main() {
-    System.out.println("Set up project");
-    // Ask to keep Git hosting files
-    // Ask to keep extra documents
-    // Ask to keep this file
+    var options = List.of(
+        entry("GitHub", prompt("Keep GitHub workflows and templates: (Yn)", "y")),
+        entry("GitLab", prompt("Keep GitLab workflows and templates: (Yn)", "y")),
+        entry("GitLab", prompt("Keep 'CODE_OF_CONDUCT.md' file: (Yn)", "y")),
+        entry("GitLab", prompt("Keep 'CONTRIBUTING.md' file: (Yn)", "y")),
+        entry("GitLab", prompt("Keep 'LICENSE.md' file: (Yn)", "y")),
+        entry("GitLab", prompt("Keep this set up file 'set_up.java' file: (Yn)", "y"))
+    );
+
     // Rename artifacts, groups or base package
     // Restart Git history (or delete it)
+}
+
+public String prompt(String message, String defaultValue) {
+    console.printf(message);
+    var v = console.readLine();
+    return v == null || v.isBlank() ? defaultValue : v;
 }
